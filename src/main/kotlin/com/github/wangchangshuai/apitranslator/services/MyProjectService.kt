67380db42df1e52f28ced5c7340bfeca57a4960e
GitHub Action <action@github.com>
2023-12-23 06:30:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangchangshuai/apitranslator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangchangshuai/apitranslator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangchangshuai.apitranslator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangchangshuai.apitranslator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
